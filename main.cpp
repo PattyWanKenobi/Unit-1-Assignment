0f9f56d08d0abde48716e5cee0078cfd17c6a456
PattyWanKenobi <93097839+PattyWanKenobi@users.noreply.github.com>
2021-12-08 07:54:15 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,3 +1,8 @@
+//Date Created: 10/20/2021
+//Tarrant County College/Dr.Tyson McMillan
+//A program to practice setting up function prototypes, headers, and definitions
+
+
 #include <iostream>
 #include <string>
 #include "Functions.h"
